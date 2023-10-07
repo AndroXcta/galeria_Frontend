967722cfd394bde44396ce506a46de1d372be735
Alejandro_Carmona <alejocarmonah@gmail.com>
2023-10-07 12:27:35 -0500

    tengo pendiente arreglar el boton para salir del formulario

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-import './style.css'
+import "./style.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const overlay = document.querySelector("nav");
+//   const sidebar = document.querySelector(".is-active");
+  const navbarToggle = document.querySelector(".containerOne");
+  const menuBtn = document.querySelector(".hamburger-box");
+
+  menuBtn.addEventListener("click", () => {
+    overlay.classList.toggle("is-open");
+    menuBtn.parentElement.classList.toggle("is-active");
+  });
+});
 
