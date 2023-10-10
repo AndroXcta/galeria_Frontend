df9a0898fed92072d38d05abee6ba86560007b1a
AndroXcta <alejocarmonah@gmail.com>
2023-10-10 11:36:18 -0500

    tengo q empezar con el metodo fetch

--- main.js
+++ main.js
@@ -12,3 +12,24 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+
+const link_form = document.querySelector(".crear_cuadro")
+const btn_form = document.querySelector(".form")
+
+link_form.addEventListener("click", () => {
+btn_form.classList.toggle("in_active")
+})
+
+
+const api = "https://jsonplaceholder.typicode.com/users"
+
+fetch(api)
+  .then (response => response.json())
+  .then (data => {
+
+    console.log(data);
+  })
+
+  .catch(error => {
+    console.error('Error al obtener datos:', error);
+  });
