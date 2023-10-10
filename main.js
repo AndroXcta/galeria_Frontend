6618c593234bb7b798abaacca900edc96f959cb1
Alejandro_Carmona <alejocarmonah@gmail.com>
2023-10-10 17:30:22 -0500

    tengo que solucionar el problema de la colision de las cajas ademas tambien tengo que arreglar el tamaño de las cartas al darle click

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ import "./style.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   const overlay = document.querySelector("nav");
-//   const sidebar = document.querySelector(".is-active");
-  const navbarToggle = document.querySelector(".containerOne");
+  navbarToggle = document.querySelector(".containerOne");
   const menuBtn = document.querySelector(".hamburger-box");
 
   menuBtn.addEventListener("click", () => {
@@ -33,3 +32,35 @@ fetch(api)
   .catch(error => {
     console.error('Error al obtener datos:', error);
   });
+
+
+  const d = document;
+  const botones = d.querySelectorAll(".containerCard");
+  
+  botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+      const instancias = d.querySelectorAll(".click");
+      instancias.forEach(instancia => instancia.classList.remove("click"));
+      
+      const div = d.createElement("div");
+      div.className = "click";
+  
+      if (!boton.classList.contains("click")) {
+        boton.classList.add("click");
+        document.body.appendChild(div);
+      }
+    });
+  });
+
+  fetch("https://hobart-redback-xzed.2.ie-1.fl0.io")
+  .then (response => response.json)
+  .then(data => {
+    console.log(data)
+  })
+  .catch (error => {
+
+    console.error('error:', error)
+  })
+
+
+
