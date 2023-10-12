6edd45d5a1fb57326dd595fe46369980c348fbc8
Fabian Gomez <fabian.gomez@somospacifico.org>
2023-10-12 15:05:04 -0500

    Arreglando problemas de modelo de caja y formulario para editar

--- main.js
+++ main.js
@@ -1,45 +1,43 @@
 import "./style.css";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const overlay = document.querySelector("nav");
+  const overlay = document.querySelector("aside");
   const navbarToggle = document.querySelector(".containerOne");
   const menuBtn = document.querySelector(".hamburger");
 
   menuBtn.addEventListener("click", () => {
     overlay.classList.toggle("is-open");
     navbarToggle.classList.toggle("is-active");
-    menuBtn.classList.toggle("is-active")
+    menuBtn.classList.toggle("is-active");
   });
 });
 
-
-
-const link_form = document.querySelector(".crear_cuadro")
-const btn_form = document.querySelector(".form")
+const link_form = document.querySelector(".crear_cuadro");
+const btn_form = document.querySelector(".form");
 
 link_form.addEventListener("click", () => {
-btn_form.classList.toggle("in_active")
-})
+  btn_form.classList.toggle("in_active");
+});
 
 const d = document;
 const botones = document.querySelectorAll(".containerCard");
-  const btn_edit = document.querySelectorAll(".edit");
+const btn_edit = document.querySelectorAll(".edit");
 
-  botones.forEach(boton => {
-    boton.addEventListener("click", (event) => {
-      event.stopPropagation();
+botones.forEach((boton) => {
+  boton.addEventListener("click", (event) => {
+    event.stopPropagation();
 
-      const instancias = document.querySelectorAll(".click");
-      instancias.forEach(instancia => {
-        if (!instancia) {
-          instancia.classList.remove("click");
-        }
-      });
-
-      btn_edit.forEach(editButton => {
-        editButton.classList.remove("off");
-      });
-
-      boton.classList.toggle("click");
+    const instancias = document.querySelectorAll(".click");
+    instancias.forEach((instancia) => {
+      if (!instancia) {
+        instancia.classList.remove("click");
+      }
     });
+
+    btn_edit.forEach((editButton) => {
+      editButton.classList.remove("off");
+    });
+
+    boton.classList.toggle("click");
   });
+});
